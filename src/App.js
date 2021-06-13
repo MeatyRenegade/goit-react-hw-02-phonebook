03c53ce843a9d04824f99afb4eb0e786b1908054
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-06-14 00:54:21 +0300

    Добавленна тестовая разметка

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Hello World</header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <h1>Phonebook</h1>
+          <form>
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </form>
+          <button type="submit">Add contact</button>
+          <ul>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
