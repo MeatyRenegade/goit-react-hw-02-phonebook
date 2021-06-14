62ec8ac0ce99cd10609d7d341bdaa057f0870d7a
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-06-14 22:55:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,31 +2,47 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  // state = {
+  //   contacts: [],
+  //   name: '',
+  // };
+
   state = {
-    contacts: [],
-    name: '',
+    value: '',
+  };
+
+  handleChange = e => {
+    this.setState({ value: e.target.value });
   };
 
   render() {
+    const { value } = this.state;
+
     return (
       <>
         <div className="App">
           <h1>Phonebook</h1>
           <form>
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-              required
-            />
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                required
+                value={value}
+                onChange={this.handleChange}
+              />
+            </label>
           </form>
+
           <button type="submit">Add contact</button>
           <ul>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
+            <li>Name Surname</li>
+            <li>Name Surname</li>
+            <li>Name Surname</li>
           </ul>
         </div>
       </>
