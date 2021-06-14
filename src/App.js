753058e0f5453cd719625521550e672cf91ef5ae
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-06-15 00:57:33 +0300

    Добавлены стили кнопок

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  // state = {
-  //   contacts: [],
-  //   name: '',
-  // };
-
   state = {
-    value: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
   };
 
-  handleChange = e => {
-    this.setState({ value: e.target.value });
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
   };
 
   render() {
-    const { value } = this.state;
+    const { contacts } = this.state;
 
     return (
       <>
@@ -32,17 +35,25 @@ class App extends Component {
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
                 required
-                value={value}
                 onChange={this.handleChange}
               />
             </label>
           </form>
 
           <button type="submit">Add contact</button>
-          <ul>
-            <li>Name Surname</li>
-            <li>Name Surname</li>
-            <li>Name Surname</li>
+          <ul className="ContactList">
+            {contacts.map(contact => (
+              <li className="ContactList_item" key={contact.id}>
+                <p className="ContactList_text">{contact.name}</p>
+                <p className="ContactList_text">{contact.number}</p>
+                <button
+                  className="button"
+                  onClick={() => this.deleteContact(contact.id)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
       </>
