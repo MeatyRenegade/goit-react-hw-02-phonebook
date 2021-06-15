69a65a46a36a75f25048cbef843821c298d36713
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-06-16 01:43:44 +0300

    OMG done.....

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Component } from 'react';
-import './App.css';
+import shortid from 'shortid';
 
-const INITIAL_STATE = {
-  name: '',
-  number: '',
-};
+import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
+
+import styles from './App.module.css';
 
 class App extends Component {
   state = {
@@ -14,8 +15,43 @@ class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    name: '',
-    number: '',
+    filter: '',
+  };
+
+  contactCheck = subContact => {
+    const value = subContact.name.toLowerCase();
+    const nameCheck = this.state.contacts.find(contact =>
+      contact.name.toLowerCase().includes(value),
+    );
+
+    nameCheck
+      ? alert(`${nameCheck.name} is already in contacts`)
+      : this.addContact(subContact);
+  };
+
+  addContact = subContact => {
+    const contact = {
+      id: shortid.generate(),
+      ...subContact,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+  handleChange = e => {
+    const { value } = e.currentTarget;
+    this.setState({ filter: value });
+  };
+
+  handleFilterRender = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
   };
 
   deleteContact = id => {
@@ -24,71 +60,22 @@ class App extends Component {
     }));
   };
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log(this.state);
-    this.formReset();
-  };
-
-  formReset = () => {
-    this.setState({ ...INITIAL_STATE });
-  };
-
   render() {
-    const { contacts } = this.state;
+    const { filter } = this.state;
+    const filteredContacts = this.handleFilterRender();
 
     return (
       <>
-        <div className="App">
-          <h1>Phonebook</h1>
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name{' '}
-              <input
-                type="text"
-                name="name"
-                placeholder="Enter name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-                required
-                value={this.state.name}
-                onChange={this.handleChange}
-              />
-            </label>
-            <label>
-              Number{' '}
-              <input
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-                required
-                value={this.state.number}
-                onChange={this.handleChange}
-              />
-            </label>
-            <button type="submit">Add contact</button>
-          </form>
+        <div className={styles.App}>
+          <h1 className={styles.header}>Phonebook</h1>
+          <ContactForm onSubmit={this.contactCheck} />
 
-          <ul className="ContactList">
-            {contacts.map(contact => (
-              <li className="ContactList_item" key={contact.id}>
-                <p className="ContactList_text">{contact.name}</p>
-                <p className="ContactList_text">{contact.number}</p>
-                <button
-                  className="button"
-                  onClick={() => this.deleteContact(contact.id)}
-                >
-                  Delete
-                </button>
-              </li>
-            ))}
-          </ul>
+          <h2 className={styles.header}>Contacts</h2>
+          <Filter value={filter} onChange={this.handleChange} />
+          <ContactList
+            filteredContacts={filteredContacts}
+            onDeleteContact={this.deleteContact}
+          />
         </div>
       </>
     );
